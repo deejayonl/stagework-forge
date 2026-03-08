a8bc5644a75ba3bce090852ef88266a2aecf9213
Coder <coder@gio-ws>
2026-03-08 14:22:25 +0000

    Cleanup temp scripts

--- patch_pi_props.sh
+++
@@ -1,2 +0,0 @@
-sed -i 's/onUpdateAttribute?: (attr: string, value: string) => void;/onUpdateAttribute?: (attr: string, value: string) => void;\n  onChangeTag?: (tag: string) => void;/g' src/components/PropertyInspector.tsx
-sed -i 's/onUpdateAttribute,/onUpdateAttribute,\n  onChangeTag,/g' src/components/PropertyInspector.tsx
--- patch_workspace_props.sh
+++
@@ -1,1 +0,0 @@
-sed -i 's/onUpdateAttribute={handleUpdateAttribute}/onUpdateAttribute={handleUpdateAttribute}\n            onChangeTag={handleChangeTag}/g' src/components/Workspace.tsx
